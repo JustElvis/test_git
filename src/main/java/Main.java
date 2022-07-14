70c6c9a9693a9d5c01501178d24e26a0fc3f6823
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2022-07-14 14:10:47 +0300

    added methods sum() and div()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    public static int sum(int a, int b){
+        return a + b;
+    }
+    public static int dev(int a, int b){
+        return a - b;
+    }
 }
